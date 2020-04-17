0c806816fbd24b87537c3518d8199927f42477d6
Jason Haddix <jasonhaddix@Jasons-MacBook-Pro.local>
2020-04-17 11:54:34 -0500

    fix divIcon issue with motion

--- development/web-app/src/app/map/leaflet/leaflet.component.ts
+++ development/web-app/src/app/map/leaflet/leaflet.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, OnChanges, Input } from '@angular/core';
-import { icon, latLng, Map, marker, point, polyline, tileLayer, Layer } from 'leaflet';
 import * as L from 'leaflet';
 import '../../../../node_modules/leaflet.motion/dist/leaflet.motion.js';
 import '@elfalem/leaflet-curve';
@@ -13,15 +12,14 @@ import '@elfalem/leaflet-curve';
 export class LeafletComponent implements OnChanges {
 
   @Input() map_data;
-  map: Map;
-  markers: Layer[] = [];
-
+  map: L.Map;
+  
   // Define our base layers so we can reference them multiple times
-  darkMode = tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+  darkMode = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
     maxZoom: 20,
 	  attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
   });
-  wMaps = tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
+  wMaps = L.tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
     detectRetina: true,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   });
@@ -34,131 +32,146 @@ export class LeafletComponent implements OnChanges {
   };
 
 
-  route_speed = 3000;
-
-  
-
-  routes:any;
-  
   // Set the initial set of displayed layers (we could also use the leafletLayers input binding for this)
   options = {
     layers: [ this.darkMode], //, this.route, this.summit, this.paradise 
     zoom: 10,
-    center: latLng([ 41.15444, -96.04225 ])//  
+    center: L.latLng([ 41.15444, -96.04225 ])//  
   };
+
+  
+  markers: L.Layer[] = [];
+  route_speed = 3000;
+  routes:any;
+  seqGroup:any;
+  overlays:any;
 
  
 
   constructor() { }
 
-  onMapReady(map: Map) {
+  onMapReady(map: L.Map) {
     this.map = map;
-    // map.fitBounds(this.markers.getBounds(), {
-    //   padding: point(24, 24),
-    //   maxZoom: 12,
-    //   animate: true
-    // });
   }
 
   ngOnChanges() {    
-    
-    var delay_bomb = 0;
-    var overlays = {};
+        
+    this.overlays = {};
     this.routes = [];
     if(this.map_data){
       
       this.map_data.locations.forEach( (location, index) => {
-        const new_marker = marker(location.coordinates[0], {
-          icon: icon({
-            iconSize: [ 25, 41 ],
-            iconAnchor: [ 13, 41 ],
-            iconUrl: 'leaflet/marker-icon.png',
-            shadowUrl: 'leaflet/marker-shadow.png'
+        var new_marker = L.marker(location.coordinates[0], {
+          icon: L.divIcon({
+            className: 'custom-div-icon',
+            html: "<i class='fas fa-map-pin' style='color:red; font-size: 20px;'></i>",
+            iconSize: [30, 42],
+            iconAnchor: [5, 20]
           })
         });
 
         this.markers.push(new_marker);
 
-        const route = L.motion.polyline(location.coordinates, {
-          color: "#" + Math.floor(Math.random()*16777215).toString(16),
+        var route = L.motion.polyline(location.coordinates, {
+          color: "steelblue",// + Math.floor(Math.random()*16777215).toString(16),
           weight:4
-        },  {
+        }, 
+        {          
+          auto:false,
+          duration:this.route_speed
+        },
+        {
           removeOnEnd: true,
-          showMarker: false,
-          // icon: L.divIcon({html: "<i class='fa fa-car fa-2x' aria-hidden='true'></i>", iconSize: L.point(27.5, 24)})
-        }).motionDuration(this.route_speed);
-        delay_bomb += this.route_speed;
+          icon: L.divIcon({
+            className: 'custom-div-icon',
+            html: "<i class='fas fa-map-pin' style='color:red; font-size: 20px;'></i>",
+            iconSize: [30, 42],
+            iconAnchor: [5, 20]
+          })
+        }).addTo(this.map);
+
 
         this.routes.push(route);
 
-        // route.addTo(this.map);
-
-        overlays[location.name] = route;
+        this.overlays[location.name] = route;
         
                 
       });  
       
 
-      var seqGroup = L.motion.seq(this.routes).addTo(this.map);
-      seqGroup.motionStart();
-
-      
-
-
-      setTimeout(() =>{
-        
-        this.map.fitBounds([[41.15444, -96.04225],[50.14874640066278,14.106445312500002]], {
-          padding: point(24, 24),
-          maxZoom: 12,
-          animate: true
-        });
-        
-        this.options = {
-          layers: [ this.darkMode], //, this.route, this.summit, this.paradise 
-          zoom: 10,
-          center: latLng([ 46.86019101567027,-29.047851562500004  ])//  41.15444, -96.04225
-        };
-
-        var pathOne = L.curve([
-          'M',[50.14874640066278,14.106445312500002],
-          'Q', this.get_midpoint([50.14874640066278,14.106445312500002], [41.15444, -96.04225], 'up'),//[59.777859, -44.007279],
-          [41.15444, -96.04225]
-        ], {animate: 2000}).addTo(this.map);
-        
-      
-        
-        setTimeout(() =>{
-          var circle = L.circleMarker(
-            [41.15444, -96.04225], 
-            { radius: 15, color: 'red', fillOpacity: 0.6, weight: 1 }) 
-            .addTo(this.map);
-  
-  
-          var newRadius = 100;
-          var interval = setInterval(function() {
-            var currentRadius = circle.getRadius();
-            console.debug("currentRadius", currentRadius)
-            if (currentRadius < newRadius) {
-                circle.setRadius(++currentRadius);
-                console.debug("new ", circle.getRadius())
-            } else {
-                clearTimeout(interval);
-            }
-          }, 1);
-  
-        }, 2000);
-
-      }, delay_bomb);          
+      this.seqGroup = L.motion.seq(this.routes).addTo(this.map);
 
       
     }
-    this.layersControl["overlays"] = overlays;
-    
-    
-
+    this.layersControl["overlays"] = this.overlays;
+  
 
   }
 
+
+  
+  start_draw_curve(){        
+      
+    this.map.fitBounds([[41.15444, -96.04225],[50.14874640066278,14.106445312500002]], {
+      padding: L.point(24, 24),
+      maxZoom: 12,
+      animate: true
+    });
+  
+
+    var pathOne = L.curve([
+      'M',[50.14874640066278,14.106445312500002],
+      'Q', this.get_midpoint([50.14874640066278,14.106445312500002], [41.15444, -96.04225], 'up'),//[59.777859, -44.007279],
+      [41.15444, -96.04225]
+    ], {animate: 2000});
+    
+    this.overlays["curve"] = pathOne;
+    this.layersControl["overlays"] = this.overlays;
+
+    setTimeout(() =>{
+      pathOne.addTo(this.map);
+    }, 500);
+    
+  }
+  
+  
+  start_draw(){
+    this.map.fitBounds([[41.15444, -96.04225],[41.15444, -96.04225]], {
+      padding: L.point(24, 24),
+      maxZoom: 10,
+      animate: true
+    });
+    this.seqGroup.motionStart();
+    // this.routes.forEach( (route, index) => {
+    //   route.motionStart();
+    // });      
+  }
+
+  pause_draw(){    
+    this.seqGroup.motionPause();  
+  }
+
+  resume_draw(){    
+    this.seqGroup.motionResume(); 
+  }
+
+  stop_draw(){    
+    this.seqGroup.motionStop(); 
+  }
+
+  speedup_draw(){    
+    this.route_speed = this.route_speed - 500;
+    this.routes.forEach( (route, index) => {
+      route.motionDuration(this.route_speed);
+    });  
+  }
+
+  slowdown_draw(){    
+    this.route_speed = this.route_speed + 500;
+    this.routes.forEach( (route, index) => {
+      route.motionDuration(this.route_speed);
+    });  
+  }
 
 
 
@@ -192,45 +205,109 @@ export class LeafletComponent implements OnChanges {
     return midpointLatLng;
   }
 
-  start_draw(){
-    this.routes.forEach( (route, index) => {
-      route.motionStart();
-    });      
-  }
 
-  pause_draw(){    
-    this.routes.forEach( (route, index) => {
-      route.motionPause();
-    });  
-  }
+  tell_a_story(){
+    this.overlays = {};
+    this.routes = [];
+    this.routes = [];
+    this.map.fitBounds([[34.038474, -118.252141],[38.631101, -80.651906]], {
+      padding: L.point(24, 24),
+      maxZoom: 5,
+      animate: true
+    });
+    if(this.map_data){
+      console.log("TELL A STORY")
+      this.map_data.story.forEach( (location, index) => {
+        const new_marker = L.marker(location.coordinates[0], {
+          icon: L.divIcon({
+            className: 'custom-div-icon',
+            html: "<i class='fas fa-car' style='color:red; font-size: 20px;'></i>",
+            iconSize: [30, 42],
+            iconAnchor: [5, 20]
+          })
+        });
 
-  resume_draw(){    
-    this.routes.forEach( (route, index) => {
-      route.motionResume();
-    });  
-  }
+        this.markers.push(new_marker);
 
-  stop_draw(){    
-    this.routes.forEach( (route, index) => {
-      route.motionStop();
-    });  
-  }
+        const route = L.motion.polyline(location.coordinates, {
+          color: "#" + Math.floor(Math.random()*16777215).toString(16),
+          weight:4
+        },  {          
+          auto:true,
+          duration:this.route_speed
+        },
+        {
+          removeOnEnd: true,
+          icon: L.divIcon({
+            className: 'custom-div-icon',
+            html: "<i class='fas fa-car' style='color:red; font-size: 20px;'></i>",
+            iconSize: [30, 42],
+            iconAnchor: [5, 20]
+          })
+        }).motionDuration(this.route_speed);
 
-  speedup_draw(){    
-    this.route_speed = this.route_speed - 500;
-    console.log(this.route_speed);
-    this.routes.forEach( (route, index) => {
-      route.motionDuration(this.route_speed);
-      route.motionStart();
-    });  
-  }
+        this.routes.push(route);
 
-  slowdown_draw(){    
-    this.route_speed = this.route_speed + 500;
-    this.routes.forEach( (route, index) => {
-      route.motionDuration(this.route_speed);
-      route.motionStart();
-    });  
+        // route.addTo(this.map);
+
+        this.overlays[location.name] = route;
+        
+                
+      });  
+      
+
+      this.seqGroup = L.motion.seq(this.routes).addTo(this.map);
+      this.seqGroup.motionStart();
+      
+    }
+    this.layersControl["overlays"] = this.overlays;
+
+    var pathOne = L.curve([
+      'M',[38.63402,- 79.83697],
+      'Q', this.get_midpoint([38.63402,- 79.83697], [36.187023, -115.255986], 'up'),//[59.777859, -44.007279],
+      [36.187023, -115.255986]
+    ], {animate: 2000}).addTo(this.map);
+    
+    this.overlays["curve"] = pathOne;
+    this.layersControl["overlays"] = this.overlays;
+
+
+    // this.map.fitBounds([[41.15444, -96.04225],[50.14874640066278,14.106445312500002]], {
+    //   padding: point(24, 24),
+    //   maxZoom: 12,
+    //   animate: true
+    // });
+
+
+    // var pathOne = L.curve([
+    //   'M',[50.14874640066278,14.106445312500002],
+    //   'Q', this.get_midpoint([50.14874640066278,14.106445312500002], [41.15444, -96.04225], 'up'),//[59.777859, -44.007279],
+    //   [41.15444, -96.04225]
+    // ], {animate: 2000}).addTo(this.map);
+    
+    // this.overlays["curve"] = pathOne;
+    // this.layersControl["overlays"] = this.overlays;
+    
+    // setTimeout(() =>{
+    //   var circle = L.circleMarker(
+    //     [41.15444, -96.04225], 
+    //     { radius: 15, color: 'red', fillOpacity: 0.6, weight: 1 }) 
+    //     .addTo(this.map);
+
+
+    //   var newRadius = 100;
+    //   var interval = setInterval(function() {
+    //     var currentRadius = circle.getRadius();
+    //     console.debug("currentRadius", currentRadius)
+    //     if (currentRadius < newRadius) {
+    //         circle.setRadius(++currentRadius);
+    //         console.debug("new ", circle.getRadius())
+    //     } else {
+    //         clearTimeout(interval);
+    //     }
+    //   }, 1);
+
+    // }, 2000);
   }
 
 }
