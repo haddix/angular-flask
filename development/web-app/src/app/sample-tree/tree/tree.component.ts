585ceb74ebd7eea02fcde566407a7c34ec052d83
Jason Haddix <jasonhaddix@Jasons-MacBook-Pro.local>
2020-05-01 09:31:25 -0500

    update trees styles

--- development/web-app/src/app/sample-tree/tree/tree.component.ts
+++ development/web-app/src/app/sample-tree/tree/tree.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { TREE_ACTIONS, KEYS, IActionMapping, ITreeOptions } from 'angular-tree-component';
+
 
 @Component({
   selector: 'app-tree',
-  template: '<tree-root [nodes]="nodes" [options]="options"></tree-root>',
+  templateUrl: './tree.component.html',
   styleUrls: ['./tree.component.css']
 })
 export class TreeComponent implements OnInit {
@@ -31,11 +33,49 @@ export class TreeComponent implements OnInit {
       ]
     }
   ];
-  options = {};
+  
+  options: ITreeOptions = {
+    displayField: 'nodeName',
+    isExpandedField: 'expanded',
+    idField: 'uuid',
+    hasChildrenField: 'nodes',
+    actionMapping: {
+      mouse: {
+        dblClick: (tree, node, $event) => {
+          if (node.hasChildren) TREE_ACTIONS.TOGGLE_EXPANDED(tree, node, $event);
+        }
+      },
+      keys: {
+        [KEYS.ENTER]: (tree, node, $event) => {
+          node.expandAll();
+        }
+      }
+    },
+    nodeHeight: 23,
+    allowDrag: (node) => {
+      return true;
+    },
+    allowDrop: (node) => {
+      return true;
+    },
+    allowDragoverStyling: true,
+    levelPadding: 10,
+    // useVirtualScroll: true, //test for large trees
+    animateExpand: true,
+    scrollOnActivate: true,
+    animateSpeed: 30,
+    animateAcceleration: 1.2,
+    // scrollContainer: document.documentElement 
+  }
   
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  tree_init(event){
+    event.treeModel.expandAll();
   }
 
 }
